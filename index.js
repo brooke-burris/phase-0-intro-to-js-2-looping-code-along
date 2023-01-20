8e0706e82dd920ec7481135c8b80e6d4f800020e
Brooke <brooke-burris@outlook.com>
2023-01-19 20:34:42 -0600

    commit tree 8262bc18bfb8c8c73263931124b92735ccf7b653

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+//for (let age = 30; age < 40; age++) {
+  //  console.log(`I'm ${age} years old. Happy birthday to me!`);
+//}
+
+//const gifts = ["teddy bear", "drone", "doll"];
+
+//function wrapGifts(gifts) {
+  //  for(let i=0; i < gifts.length; i++) {
+    //console.log (`Wrapped ${gifts[i]} and added a bow!`);
+//}
+//return gifts;
+//} 
+//wrapGifts(gifts);
+
+function writeCards(name, event){
+    let cards = []
+for (let i=0; i < name.length; i++) {
+    cards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+}
+return cards
+}
+
+function countDown(countDown) {
+   while(countDown > 0) {
+       console.log(countDown);
+       countDown -= 1;
+}
+    console.log(countDown)
+}
